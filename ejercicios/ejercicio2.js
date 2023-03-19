33009654abdeabd58a4756d7efc2ddec96ef625e
Lisandro Coronel <lisandrojulian2004@gmail.com>
2023-03-19 19:07:23 -0300

    hice arreglos pequeños en el ejercicio 2

--- ejercicios/ejercicio2.js
+++ ejercicios/ejercicio2.js
@@ -10,10 +10,10 @@ while (true) {
 
 ciudades.push("París");
 
-document.write("<p>La longitud del arreglo es: " + ciudades.length + "</p>");
-document.write("<p>El primer elemento es: " + ciudades[0] + "</p>");
-document.write("<p>El tercer elemento es: " + ciudades[2] + "</p>");
-document.write("<p>El último elemento es: " + ciudades[ciudades.length - 1] + "</p>");
+document.write("<p>La longitud del arreglo: " + ciudades.length + "</p>");
+document.write("<p>Elemento 1er posicion: " + ciudades[0] + "</p>");
+document.write("<p>Elemento 3er posicion: " + ciudades[2] + "</p>");
+document.write("<p>Elemento ultima posicion: " + ciudades[ciudades.length - 1] + "</p>");
 
 ciudades[1] = "Barcelona";
 
@@ -23,6 +23,6 @@ document.write("<p>El arreglo actualizado es: " + ciudades.join(", ") + "</p>");
 document.write("<h1>Arreglo de ciudades</h1>");
 document.write("<ul>");
 ciudades.forEach((ciudad, index) => {
-  document.write("<li>Elemento " + (index + 1) + ": " + ciudad + "</li>");
+  document.write("<li>Elemento " + ": " + ciudad + "</li>");
 });
 document.write("</ul>");
